7a925c9cfc9e70cc46be2d0d2212e8096126abc9
frozens <453430397@qq.com>
2021-07-01 10:43:32 +0800

    fix model session close

--- tools/studioctl/model/template.go
+++ tools/studioctl/model/template.go
@@ -2,7 +2,7 @@ package model
 
 import "fmt"
 
-var tpl_model = fmt.Sprintf(`package model
+var s = `package model
 {{if .HasImport}}
 import "go-studio/core/jsontime"
 {{end}}
@@ -76,4 +76,102 @@ func (m *{{.TableMapperName}}Model) List() (list []*{{.TableMapperName}}, err er
 	return
 }
 
+`
+
+var tpl_model = fmt.Sprintf(`package model
+
+import "xorm.io/xorm"
+{{if .HasImport}}
+import "gitee.com/bethink1501/24on-library/lib/jsontime"
+{{end}}
+type (
+	{{.TableMapperName}}Model struct {
+		db *xorm.Engine
+		session *xorm.Session
+	}
+	
+	{{.TableMapperName}} struct {
+{{range .Columns}}		{{.MapperName}}	{{.Type}}	%s{{.Tag}} json:"{{.Name}}"%s // {{.Comment}}
+{{end}}	}
+)
+
+func New{{.TableMapperName}}Model(db *xorm.Engine) *{{.TableMapperName}}Model {
+	return &{{.TableMapperName}}Model{db:db}
+}
+
+func ({{.TableMapperName}}) TableName() string {
+	return "{{.TableName}}"
+}
+
+func (m *{{.TableMapperName}}Model) WithSession(session *xorm.Session) *{{.TableMapperName}}Model {
+	return &{{.TableMapperName}}Model{db:m.db, session: session}
+}
+
+
+func (m *{{.TableMapperName}}Model) Insert(data *{{.TableMapperName}}) error {
+	var session =m.session
+	if session ==nil {
+		session = m.db.NewSession()
+		defer session.Close()
+	}
+
+	affect, err := session.InsertOne(data)
+	if err != nil {
+		return err
+	}
+	if affect != 1 {
+		return errcode.DBUpdateNotAffected
+	}
+	return nil
+}
+
+func (m *{{.TableMapperName}}Model) FindOne(id int64) (*{{.TableMapperName}}, error) {
+	var resp {{.TableMapperName}}
+	has, err := m.db.ID(id).Get(&resp)
+	if err != nil {
+		return nil, err
+	}
+	if !has {
+		return nil, errcode.DBDataNotFound
+	}
+	return &resp, nil
+}
+
+func (m *{{.TableMapperName}}Model) Update(data *{{.TableMapperName}}) error {
+	var session =m.session
+	if session ==nil {
+		session = m.db.NewSession()
+		defer session.Close()
+	}
+
+	_, err := session.ID(data.Id).Update(data)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (m *{{.TableMapperName}}Model) Delete(id int64) error {
+	var session =m.session
+	if session ==nil {
+		session = m.db.NewSession()
+		defer session.Close()
+	}
+
+	affect, err := session.ID(id).Delete({{.TableMapperName}}{})
+	if err != nil {
+		return err
+	}
+	if affect != 1 {
+		return errcode.DBUpdateNotAffected
+	}
+	return nil
+}
+
+func (m *{{.TableMapperName}}Model) List() (list []*{{.TableMapperName}}, err error) {
+	list = make([]*{{.TableMapperName}}, 0)
+	err = m.db.Find(&list)
+	return
+}
 `, "`", "`")
